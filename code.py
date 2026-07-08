33dcf42498a1d065a4ef1ff1a222a774a3759294
shreyaspoojari26 <shreyaspoojari900@gmail.com>
2026-07-08 21:52:00 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,15 @@
+#To input two numbers
+a=int(input("Enter first number:")
+b=int(input("Enter second number:')
+#To print number before swapping
+print("Before interchanging")
+print("value ofa=",a)
+print("value of b=",b)
+#TO interchanging two numbers
+temp=a
+a=base=
+b=temp
+#To print number after swapping
+print("After interchanging")
+print("value of a=",a)
+print("value of b=",b)
