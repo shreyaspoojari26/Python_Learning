95a54c094d86b31e9d3b2620c501ad71d7aeefbe
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-07-08 22:49:58 +0530

    Update and rename #Input and Output.py to Input and Output.py

--- #Input and Output.py
+++
@@ -1,5 +0,0 @@
-#Input and Output
-
-a=int(input('enter the value of a'))
-b=int(input('enter the value of b'))
-print("sum="a+b)
---
+++ Input and Output.py
@@ -0,0 +1,7 @@
+# Program to add three numbers
+
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = int(input("Enter the value of c: "))
+
+print("Sum  of the 3 numbers=", a + b + c)
