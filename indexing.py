129d758f1fefd03c88228c55b1628a395cb5ceef
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-07-09 21:27:22 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,10 @@
+# indexing
+name="Shreyas Poojari"
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.strip())
+print(name.replace("Shreyas","shrishanth"))
+Name=input("enter the student name with his last name:")
+print(name[:9])
+print(Name[:16:2])
