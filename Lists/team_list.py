0cd5a0538dd474d9ee45561f36b533e6ebc1b5f6
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-07-11 22:40:37 +0530

    Create team_list.py

---
+++ Lists/team_list.py
@@ -0,0 +1,19 @@
+### Team_List
+team_players=['Rohit Sharma','Virat Kohli','rahul','Dhoni','Pant','DK','Jadeja']
+print(team_players)
+team_players.append("Siraj")
+print("Substitute Player::",team_players)
+team_players.remove("Pant")
+print("available players: ",team_players)
+team_players.insert(0,"Gill")
+print("Captain of the team:",team_players)
+team_players.reverse()
+print(team_players)
+print(team_players[::-1])
+print(team_players)
+print(len(team_players))
+print(team_players)
+print(team_players[0:6])
+team_players.reverse()
+print(team_players)
+
