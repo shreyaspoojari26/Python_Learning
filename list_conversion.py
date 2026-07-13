2df0c555875e8eb05f94894dfdb558d7e7333fa8
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-07-13 22:32:36 +0530

    Create list_conversion.py

---
+++ list_conversion.py
@@ -0,0 +1,17 @@
+numbers = (10, 20, 30, 40)
+
+numbers_list = list(numbers)
+
+print(numbers_list)
+
+
+
+
+numbers = (10, 20, 30, 40, 50)
+
+n = int(input("Enter number: "))
+
+if n in numbers:
+    print("Found")
+else:
+    print("Not Found")
