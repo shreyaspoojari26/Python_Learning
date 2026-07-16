fbdb8283a5da8040d4ef0b14b53f0bbf86b39231
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-07-16 22:19:02 +0530

    Create practiced.py

---
+++ practiced.py
@@ -0,0 +1,10 @@
+marks = [78, 92, 65, 88, 95]
+
+print("Marks:", marks)
+print("Highest Mark:", max(marks))
+print("Lowest Mark:", min(marks))
+print("Total Marks:", sum(marks))
+print("Average Marks:", sum(marks) / len(marks))
+
+marks.sort()
+print("Sorted Marks:", marks)
