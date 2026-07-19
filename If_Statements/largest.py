49e0a1ac8a449cf86f58a7640e6638fe26a67757
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-07-19 22:16:43 +0530

    Create largest.py

---
+++ If_Statements/largest.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 > num2:
+    print("Largest Number:", num1)
+
+elif num2 > num1:
+    print("Largest Number:", num2)
+
+else:
+    print("Both numbers are Equal")
