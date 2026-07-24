311dbe72a7019e3f52a11a95db0f151b34f6dff5
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-07-24 21:28:32 +0530

    Update marks_grade.py

--- Conditional Statements/marks_grade.py
+++ Conditional Statements/marks_grade.py
@@ -2,12 +2,9 @@ mark = int(input("Enter the marks: "))
 
 if mark >= 90:
     print("Grade A")
-
 elif mark >= 75:
     print("Grade B")
-
 elif mark >= 50:
     print("Grade C")
-
 else:
     print("Failed")
