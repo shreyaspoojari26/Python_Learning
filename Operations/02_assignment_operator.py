4feda3ad2c74e82221e7f6a598eca6f16ecc948a
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-07-25 23:10:13 +0530

    Create 02_assignment_operator.py

---
+++ Operations/02_assignment_operator.py
@@ -0,0 +1,9 @@
+x = 15
+y = 20
+
+print(x == y)  # False  Equal to
+print(x != y)  # True   Not equal to
+print(x > y)   # False  Greater than
+print(x < y)   # True   Less than
+print(x >= 15) # True   Greater than or equal
+print(x <= 20) # True   Less than or equal
