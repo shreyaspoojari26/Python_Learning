729bb7b48dc38cf2f6e0f86303f776f3c3711353
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-07-25 23:13:31 +0530

    Create laptop.py

---
+++ Membership Operator/laptop.py
@@ -0,0 +1,7 @@
+laptops = ["HP Victus", "Dell", "Asus"]
+
+print("HP Victus" in laptops)    # True
+print("Mac" not in laptops)      # True
+
+name = "Python"
+print("y" in name)               # True
