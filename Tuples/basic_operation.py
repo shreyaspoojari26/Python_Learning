140cd7d28abe023b092fc3e833f122d474baaa45
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-07-31 22:15:22 +0530

    Create basic_operation.py

---
+++ Tuples/basic_operation.py
@@ -0,0 +1,7 @@
+fruits = ("apple", "banana", "mango", "apple")
+
+print("Tuple:", fruits)
+print("First item:", fruits[0])
+print("Count of apple:", fruits.count("apple"))
+print("Index of banana:", fruits.index("banana"))
+print("Length:", len(fruits))
