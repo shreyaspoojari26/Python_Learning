ba8486fefaa97eebd609dab32407a7a9364e9fb9
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-08-07 21:43:36 +0530

    Create min_max.py

---
+++ Tuples/min_max.py
@@ -0,0 +1,6 @@
+marks = (45, 89, 23, 67, 90)
+
+print("Highest:", max(marks)) # 90
+print("Lowest:", min(marks)) # 23
+print("Total:", sum(marks)) # 314
+print("Average:", sum(marks)/len(marks)) # 62.8
