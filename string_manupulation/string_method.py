57f6961437771dfbd85108127f95f740ea997ca6
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-08-14 21:23:33 +0530

    Create string_method.py

---
+++ string_manupulation/string_method.py
@@ -0,0 +1,8 @@
+s = " hello World "
+
+print(s.upper()) # HELLO WORLD
+print(s.lower()) # hello world
+print(s.strip()) # "hello World" - removes spaces
+print(s.replace("World", "Python")) # hello Python
+print(s.split()) # ['hello', 'World'] - makes list
+print(s.title()) # Hello World
