ceb7828aa2b001d185b1219f30251e9811fac7b3
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-08-17 21:23:52 +0530

    Create greater_than_twenty.py

---
+++ Lists/greater_than_twenty.py
@@ -0,0 +1,9 @@
+numbers = [12, 7, 25, 8, 30, 15, 4, 21, 40, 18]
+
+greater = []
+
+for item in numbers:
+    if item > 20:
+        greater.append(item)
+
+print("Numbers greater than 20:", greater)
