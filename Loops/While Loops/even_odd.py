19309c523a24d797715c2b6eb40913581bc845e9
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-08-21 22:25:04 +0530

    Create even_odd.py
    Even and odd using while loop

---
+++ Loops/While Loops/even_odd.py
@@ -0,0 +1,8 @@
+num = 1                # start at 1
+
+while num <= 10:       # keep going as long as num is 10 or less
+    if num % 2 == 0:
+        print(num, "even")
+    else:
+        print(num, "odd")
+    num += 1            # move to the next number — VERY important!
