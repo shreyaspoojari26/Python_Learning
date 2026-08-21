f67bd0855b0866f5c87ae6cffe3e8a935848f192
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-08-21 22:13:26 +0530

    Create colours.py

---
+++ Lists/colours.py
@@ -0,0 +1,4 @@
+colours = ['green', 'orange', 'red', 'blue']
+print(colours[1])              # orange
+colours.append("yellow")       
+print("updated list", colours) # updated list ['green', 'orange', 'red', 'blue', 'yellow']
