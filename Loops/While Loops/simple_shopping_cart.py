46c987c79780fb132ded73e08c147c3813c7b373
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-08-24 21:43:45 +0530

    Create simple_shopping_cart.py

---
+++ Loops/While Loops/simple_shopping_cart.py
@@ -0,0 +1,35 @@
+cart = []
+
+while True:
+
+    print("\n1. Add item")
+    print("2. View cart")
+    print("3. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+
+        item = input("Enter item: ")
+        cart.append(item)
+
+        print("Item added!")
+
+    elif choice == "2":
+
+        if len(cart) == 0:
+            print("Cart is empty")
+
+        else:
+            print("Your cart:")
+
+            for item in cart:
+                print("-", item)
+
+    elif choice == "3":
+
+        print("Thank you!")
+        break
+
+    else:
+        print("Invalid choice")
