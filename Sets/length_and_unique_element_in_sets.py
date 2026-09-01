2de905f5a9a9a725cb870cb7df221c19a49076a8
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-09-01 22:29:45 +0530

    Add example for set length and unique elements
    Initialize a set and demonstrate its properties.

---
+++ Sets/length_and_unique_element_in_sets.py
@@ -0,0 +1,10 @@
+s = set()
+s.add(20)
+s.add(20.0)
+s.add('20')
+
+# Print the final length of the set
+print("Length of set s:", len(s))
+
+# Print the unique elements inside the set
+print("Elements in set s:", s)
