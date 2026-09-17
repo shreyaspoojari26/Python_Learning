0e63c6eab8cc0f85f47ded93e979b65f0f060110
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-09-17 21:24:38 +0530

    Create ticket_price.py

---
+++ Conditional Statements/ticket_price.py
@@ -0,0 +1,11 @@
+# Checking Bus Ticket Prices
+age = 65
+
+if age < 5:
+    print("Ticket is free.")
+elif age <= 12:
+    print("You get a child discount.")
+elif age >= 60:
+    print("You get a senior citizen discount.")
+else:
+    print("You pay the full fare.")
