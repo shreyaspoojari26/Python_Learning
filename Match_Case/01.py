6846f8cf17a1c65491b1be6c9493b8359e88756c
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-09-17 21:53:43 +0530

    Add match-case statement for day of the week

---
+++ Match_Case/01.py
@@ -0,0 +1,14 @@
+day = "Sunday"
+
+match day:
+    case "Monday":
+        print("Monday")
+
+    case "Friday":
+        print("Friday")
+
+    case "Sunday":
+        print("Sunday")
+
+    case _:
+        print("Other day")
