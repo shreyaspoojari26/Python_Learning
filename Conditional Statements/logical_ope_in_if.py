bc2e53d9ea25e56c5a1c5f4060fdccfdabf732e0
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-09-17 21:25:52 +0530

    Create logical_ope_in_if.py

---
+++ Conditional Statements/logical_ope_in_if.py
@@ -0,0 +1,8 @@
+"""Logical Operators in if Statements"""
+age = 16
+has_student_id = True
+
+if age < 18 and has_student_id:
+    print("You are eligible for the student discount!")
+else:
+    print("You are not eligible for the student discount.")
