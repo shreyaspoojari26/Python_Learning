f8daa17c35fb05d98712fa7ca407ccdb2e737a90
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-09-17 21:28:35 +0530

    Create meal_time.py

---
+++ Conditional Statements/meal_time.py
@@ -0,0 +1,10 @@
+time = 15  # 3 PM
+
+if time == 8:
+    print("It's breakfast time!")
+elif time == 13:
+    print("It's lunch time!")
+elif time == 20:
+    print("It's dinner time!")
+else:
+    print("It's not a meal time.")
