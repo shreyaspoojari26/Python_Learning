55e13365f6a6c964e1c58655add5b9eb53296404
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-09-18 21:38:03 +0530

    Create while_user_input.py

---
+++ Loops/While Loops/while_user_input.py
@@ -0,0 +1,7 @@
+pin = ""
+correct_pin = "1234"
+while pin != correct_pin:
+    pin = input("Enter your PIN: ")
+    if pin != correct_pin:
+        print("Incorrect PIN. Try again.")
+print("PIN accepted. You can proceed.")
