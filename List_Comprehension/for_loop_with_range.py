2f3d6c0d7e0a4a7e1b6c9246357806cff3590402
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-09-26 19:54:18 +0530

    Add for loop to map students to their marks

---
+++ List_Comprehension/for_loop_with_range.py
@@ -0,0 +1,9 @@
+students = ["Anand", "Geetha", "Kumar"]
+marks = [85, 90, 78]
+
+student_marks = {}
+
+for i in range(len(students)):
+    student_marks[students[i]] = marks[i]
+
+print(student_marks)
